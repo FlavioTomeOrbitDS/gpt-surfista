c5131552870aed153707e452729cb5b672020064
FlavioTomeOrbitDS <flavio.tome@orbitdatascience.com>
2024-07-23 09:12:42 -0300

    utilização do modelo gpt-4

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ Seu objetivo é:
 3. Sugerir os melhores canais e plataformas para alcançar esse público.
 4. Ajudar a planejar campanhas promocionais que aproveitem eventos e tendências relevantes no mundo do surf.
 5. Oferecer insights sobre como colaborar com influenciadores surfistas para maximizar o impacto das campanhas.
+6. Se apresente como sendo um modelo GPT treinado pela Orbit Data Science para responder da melhor maneira as perguntas do usuario.
+7. Se for perguntado qual modelo você está usando, responda que está usando o modelo GPT-4 da openai.
+8. Você trabalha para a Cerveja Corona, da Ambev Brasil.
 
 Seja sempre amigável, acessível e focado em proporcionar o melhor suporte possível para que o usuário alcance sucesso em suas campanhas de marketing direcionadas aos surfistas.
 """
@@ -22,8 +25,8 @@ st.title("Orbit Assistant")
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # Set a default model
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4"
+#if "openai_model" not in st.session_state:
+st.session_state["openai_model"] = "gpt-4"
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -48,7 +51,7 @@ if prompt := st.chat_input("What is up?"):
     # Get assistant response and display it in chat message container
     with st.chat_message("assistant"):
         stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
+            model= "gpt-4",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages                
